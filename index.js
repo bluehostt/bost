e2c44a6070ee355283430cea7082330bd202d9ba
bluehostt <44468166+bluehostt@users.noreply.github.com>
2018-12-20 01:13:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -63,18 +63,7 @@ client.on('message', msg => {
     });     
 
 
- client.on('message', message => {
-if (message.content.startsWith(prefix + "obc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
+
 
 
 client.login(process.env.TOKEN);
